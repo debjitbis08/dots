f3f466d84cf560dba693b121effea2b2dd4e680e
Debjit Biswas <debjit@fastmail.fm>
2019-12-02 23:09:10 +0530

    Add global git ignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,18 @@
-set PATH (brew --prefix coreutils)/libexec/gnubin $PATH
-set PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
+# set PATH (brew --prefix coreutils)/libexec/gnubin $PATH
+set PATH $HOME/.pyenv/bin $HOME/.local/bin $HOME/.cargo/bin $PATH
 
 fundle plugin 'edc/bass'
 
 fundle init
 
 nvm use default
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /home/debjit/.nvm/versions/node/v10.15.0/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+sudo apt install vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt install fish
+chsh -s `which fish`
+
+curl -L https://get.oh-my.fish | fish
+omf install bobthefish
+sudo apt-get install fonts-powerline
+
 curl -sfL https://git.io/fundle-install | fish
 fundle install
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
-./brew.sh
+# ./brew.sh
 
 # Haskell setup
 curl -sSL https://get.haskellstack.org/ | sh
